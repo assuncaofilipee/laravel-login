69b9e7c9d1cc81786c6afb84390074db1fc8ff7c
assuncaofilipepravaler <filipe.santos@pravaler.com.br>
2022-09-19 21:37:15 -0300

    refactor: routes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -18,7 +18,7 @@ use Illuminate\Routing\Controller as BaseController;
     * )
     *
     * @OA\Server(
-    *      url=L5_SWAGGER_CONST_HOST,
+    *      url="",
     *      description="Local"
     * ),
     *
